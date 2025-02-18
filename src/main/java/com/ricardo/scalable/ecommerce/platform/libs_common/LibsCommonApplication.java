f0dd79e019d496d12c312add6fc54d0f013ff43b
RicardoRG96 <ricardoretamal10@gmail.com>
2025-02-18 06:06:03 -0300

    add common entities

--- src/main/java/com/ricardo/scalable/ecommerce/platform/libs_common/LibsCommonApplication.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/libs_common/LibsCommonApplication.java
@@ -1,13 +1,11 @@
 package com.ricardo.scalable.ecommerce.platform.libs_common;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class LibsCommonApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LibsCommonApplication.class, args);
-	}
-
+	
 }
