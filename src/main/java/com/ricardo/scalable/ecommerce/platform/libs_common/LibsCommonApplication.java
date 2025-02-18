25b5abde298d84c846e7a7ea53ede5c37d48f12d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-02-18 05:33:15 -0300

    first commit

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/libs_common/LibsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.libs_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/libs_common/LibsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.libs_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
