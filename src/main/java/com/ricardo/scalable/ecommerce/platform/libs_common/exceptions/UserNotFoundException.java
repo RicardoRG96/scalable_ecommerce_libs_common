10651e9fdb41ab941d18b104316dacc3d1138a2e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-05-15 05:22:05 -0400

    add UserNotFoundException

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/libs_common/exceptions/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ricardo.scalable.ecommerce.platform.libs_common.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    } 
+}
